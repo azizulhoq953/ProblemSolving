4607baef88183ebe81a5fecf746a179d9413ba23
azizulhoq953 <azizulhoq2041@gmail.com>
2021-12-01 22:03:35 +0600

    inputSum

---
+++ C++/hackerrankSum.cpp
@@ -0,0 +1,38 @@
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 0;
+    int sum = 0;
+
+    while (cin >> i) {
+        sum += i;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
+
+
+/*
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    // Enter your code here. Read input from STDIN. Print output to STDOUT
+
+   int a=1,b=2,c=7;
+   int sum=a+b+c;
+   cout<<sum;
+    return 0;
+}
+*/
